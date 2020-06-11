5583444b3b8c4b59cb03eb456e50e5f733f4380b
Warren Wilkinson <warrenwilkinson@gmail.com>
2020-06-11 10:44:34 -0700

    Back to where we were, grafana and influxdb

--- dev/coldstart.sh
+++ dev/coldstart.sh
@@ -76,7 +76,7 @@ sudo salt 'vagrant.vm' lxc.init salt profile=standard_bionic network_profile=sta
 # For the dev session, Shutdown the saltmaster, add in appropriate
 # mounts so we have all the salt files in the new saltmaster.
 # in production, we'd manually pull from the git repo.
-echo "Step 2. Setup Saltmaster for development environment"
+# echo "Step 2. Setup Saltmaster for development environment"
 sudo lxc-attach --name=salt -- mkdir -p /etc/salt/cloud.profiles.d
 sudo lxc-attach --name=salt -- mkdir -p /etc/salt/cloud.providers.d
 sudo lxc-attach --name=salt -- mkdir -p /etc/salt/master.d
@@ -138,65 +138,12 @@ sudo systemctl restart salt-minion
 
 # Ping should work and return
 # vagrant.vm, dns, and salt.
-$salt 'dns' test.ping
+$salt '*' test.ping
 
-# echo "Bootstrap Phase 5: Attach host to new Salt"
-# echo "########################################"
+echo "Bootstrap Phase 5: Orchestrating Cloud"
+echo "########################################"
 
-# # Stop the old master, and connect vagrant.vm to the new one.
-# sudo rm /etc/salt/pki/minion/minion_master.pub
-# sudo rm /etc/salt/minion.d/99-master-address.conf
-# sudo systemctl restart salt-minion
-# sudo systemctl stop salt-master
-# sudo systemctl disable salt-master
-
-# # Does ping work?
-# $salt 'vagrant.vm' test.ping
-
-
-# # sudo lxc-attach dns -- apt-get install openssh-server
-# # sudo lxc-attach dns -- sh -c 'echo "PermitRootLogin prohibit-password" >> /etc/ssh/sshd_config'
-# # sudo lxc-attach dns -- systemctl restart sshd
-
-# # # Make a keypair, put the key here '/tmp/mysshdnskey.pem'
-# # # and put it in authorized keys for root on the dns.
-# # ssh-keygen -N "" -f /tmp/mysshdnskey.pem -t rsa -b 4096
-# # sudo lxc-attach dns -- mkdir -p /root/.ssh
-# # sudo lxc-attach dns -- chmod 700 /root/.ssh
-# # cat "/tmp/mysshdnskey.pem.pub" | sudo lxc-attach -n dns -- /bin/sh -c "cat > /root/.ssh/authorized_keys"
-# # sudo lxc-attach dns -- chmod 644 /root/.ssh/authorized_keys
-
-# # sudo salt-cloud -p salt-dns dnsaa
-
-
-# # # Setup saltmaster:
-# # echo "Step 1. Setup new Salt master"
-# # set +e
-# # # Try this twice, it fails sometimes and I'm not sure why...
-# # sudo salt 'vagrant.vm' lxc.init salt profile=standard_bionic network_profile=standard_net bootstrap_args="-M -A salt"
-# # sudo salt 'vagrant.vm' lxc.init salt profile=standard_bionic network_profile=standard_net bootstrap_args="-M -A salt"
-# # set -e
-
-
-# # # Instantiate the cloud of the cloud:
-# # echo "Step 3. Produce cloud"
-# # sudo salt-cloud -y -m /vagrant/mapfile
-
-# # # Stop the old master, and connect vagrant.vm to the new one.
-# # sudo rm /etc/salt/pki/minion/minion_master.pub
-# # sudo rm /etc/salt/minion.d/99-master-address.conf
-# # sudo systemctl restart salt-minion
-# # sudo systemctl stop salt-master
-# # sudo systemctl disable salt-master
-
-# # # Accept all the keys
-# # sleep 90
-# # sudo lxc-attach --name=salt -- sudo salt-key -y -A
-
-# # echo "Bootstrap Phase 3: Orchestrating Cloud"
-# # echo "########################################"
-
-# # echo "Step 1. Setup monitoring infrastructure."
-# # salt="sudo lxc-attach --name=salt -- salt"
-# # $salt-run state.orchestrate _orchestrate.monitoring
-
+# Spin up all the remaining servers
+$salt-cloud -y -m /srv/salt/_orchestrate/mapfile
+$salt '*' test.ping
+$salt-run state.orchestrate _orchestrate.monitoring
